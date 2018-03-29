c322ffdbc27898042960771b3366aa44272fdd50
Venkataramana <nv.palthya@globaledgesoft.com>
2018-03-29 15:16:06 +0530

    changes in component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {Router} from '@angular/router'
+import { MatStepper } from '@angular/material';
 import { FormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { NodemodulesService } from '../nodemodules.service';
 
@@ -10,30 +11,32 @@ import { NodemodulesService } from '../nodemodules.service';
 })
 export class HomeComponent implements OnInit {
   isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
-  fourthFormGroup:FormGroup;
-
-  modules = new FormControl();
-  moduleList = ['express', 'fs', 'body-parser', 'async', 'postgres-pure', 'mongo-connect'];
+  // firstFormGroup: FormGroup;
+  // secondFormGroup: FormGroup;
+  // thirdFormGroup: FormGroup;
+  // fourthFormGroup:FormGroup;
+  search:string='';
+  @ViewChild('stepper') stepper;
+  listOfNodeModules:object=[];
+  Obj:object={};
 
   constructor(private router: Router, private _formBuilder: FormBuilder, private servicedata: NodemodulesService) { }
   someDataFromService:string = '';
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      Servercode: ['NodeJS', Validators.required],
-      Database: ['', Validators.required]
-    });
-    this.thirdFormGroup = this._formBuilder.group({
-      Nodemodules: ['', Validators.required]
-    });
-    this.fourthFormGroup = this._formBuilder.group({
-      Tablename: ['', Validators.required]
-    });
+    // this.firstFormGroup = this._formBuilder.group({
+    //   firstCtrl: ['', Validators.required]
+    // });
+    // this.secondFormGroup = this._formBuilder.group({
+    //   Servercode: ['NodeJS', Validators.required],
+    //   Database: ['', Validators.required]
+    // });
+    // this.thirdFormGroup = this._formBuilder.group({
+    //   modules: ['', Validators.required],
+    //   addedModules: ['', Validators.required]
+    // });
+    // this.fourthFormGroup = this._formBuilder.group({
+    //   Tablename: ['', Validators.required]
+    // });
   console.log(this.servicedata.testData);
   //console.log(this.servicedata.loadNodemodules());  
   this.someDataFromService = this.servicedata.loadSomeData();
@@ -43,9 +46,41 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['login']);
   }
   loadModule(){
-    this.servicedata.loadNodemodules('body-parser').subscribe((value) => {
-      console.log(value);
-    });
+    if(this.search.length>0){
+      this.servicedata.loadNodemodules(this.search).subscribe((value) => {
+        console.log(value);
+        this.listOfNodeModules = value.sections.packages;
+      });
+    }
   }
-  
+  clickItToUpdate(data:string){
+    console.log(data);
+  }
+  getAllInfoInJson(){
+    console.log(this.Obj);
+    this.stepper.reset();
+  }
+  goBack(stepper: MatStepper){
+    stepper.previous();
+  }
+
+  goForward(stepper: MatStepper, which:number){
+    console.log(which);
+    // if(which==1){
+    //   if(this.Obj.projectName){
+    //     stepper.next();
+    //   }
+    // }
+    // if(which==2){
+    //   if(this.Obj.serverSide && this.Obj.dataBase){
+    //     stepper.next();
+    //   }
+    // }
+    // if(which==3){
+    //   if(this.Obj.addedModules){
+    //     stepper.next();
+    //   }
+    // }
+    
+  }
 }
